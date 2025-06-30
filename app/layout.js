afc9b7336e7c7d40a4bf0db12bc460f59e87cb04
add000 <addthai200@gmail.com>
2025-06-30 08:17:45 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navigation from "./components/navigation";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
