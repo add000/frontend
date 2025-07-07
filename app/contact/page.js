5efadfc5f4891e40c7143e8332dc0c840aaad189
add000 <addthai200@gmail.com>
2025-07-07 12:20:03 +0700

    Update page.js

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,7 +1,7 @@
 export default function Contact() {
   return (
     <>
-    <h1 className="ftext-4xl text-center text-bule-950">Contact page</h1>
+    <h1 className="ftext-4xl text-center text-bule-950">ติดต่อเรา</h1>
     </>
   );
 }
