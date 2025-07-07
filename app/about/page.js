959a03bb0d3556f314f332b1e56662564cb74272
add000 <addthai200@gmail.com>
2025-07-07 12:19:46 +0700

    Update page.js

--- app/about/page.js
+++ app/about/page.js
@@ -1,22 +1,5 @@
-export default function About() {
-  return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-12 bg-primary text-white py-5">
-          <div className="container">
-            <h1 className="text-center">About page</h1>
-            <p className="text-center">Full-width background with centered content</p>
-          </div>
-        </div>
-      </div>
-      
-      <div className="container mt-4">
-        <div className="row">
-          <div className="col-lg-8 mx-auto">
-            <p>This content is centered and responsive</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+export default function Page() {
+    return (
+         <h1 className="ftext-4lx text-center">เกี่ยวกับเรา</h1>
+         );
 }
