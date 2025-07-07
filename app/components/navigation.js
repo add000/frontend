9038163576e9ef37c62cb5510543aea8ed153fa0
add000 <addthai200@gmail.com>
2025-07-07 12:19:58 +0700

    Update navigation.js

--- app/components/navigation.js
+++ app/components/navigation.js
@@ -1,16 +1,45 @@
-// app/components/Navigation.tsx
-'use client';
-import Link from 'next/link';
+//import ใน /app/layout.js
 
-export default function Navigation() {
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Navbar() {
   return (
-    <nav style={{ padding: '1rem', borderBottom: '1px solid #eee' }}>
-      <ul style={{ display: 'flex', gap: '1rem', listStyle: 'none' }}>
-        <li><Link href="/">หน้าแรก</Link></li>
-        <li><Link href="/about">เกี่ยวกับ</Link></li>
-        <li><Link href="/service">service</Link></li>
-        <li><Link href="/contact">ติดต่อ</Link></li>
+<nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+  <Link href="/" className="navbar-brand d-flex align-items-center gap-2"><img src="/bootstrap-logo.svg" alt="Logo" width={30} height={24} className="d-inline-block align-text-top" /> FrontEnd</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" href="/">หน้าแรก</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" href="/about">เกี่ยวกับเรา</Link>
+        </li>
+        <li className="nav-item dropdown">
+          <Link className="nav-link dropdown-toggle" href="/service" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            บริการของเรา
+          </Link>
+          <ul className="dropdown-menu">
+            <li><Link className="dropdown-item" href="/service">ติดต่อเรา</Link></li>
+            <li><Link className="dropdown-item" href="/service">ติดต่อ ด้านอื่นๆ</Link></li>
+            <li><hr className="dropdown-divider" /></li>
+            <li><Link className="dropdown-item" href="/service">สอบถามเกียวกับปัญหา</Link></li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <Link href="/contact" className="nav-link" aria-disabled="true">ติดต่อเรา</Link>
+        </li>
       </ul>
-    </nav>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="ค้นหา" aria-label="Search" />
+        <button className="btn btn-outline-warning" type="submit">ค้นหา</button>
+      </form>
+    </div>
+  </div>
+</nav>
   );
 }
