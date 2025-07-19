286f168576c7758c0d4e3749e8ffbade21510766
add000 <addthai200@gmail.com>
2025-07-19 19:21:20 +0700

    Update card.js

--- app/components/card.js
+++ app/components/card.js
@@ -2,36 +2,47 @@
 
 export default function Card() {
     return (
-    <div className="container-fluid text-light" style={{ backgroundColor: '#1F1713', padding: '20px' }}>
-        <div className="row">
-            <div className="col-md-12 text-center mb-4"></div>
-            <div className="col-md-12 text-center mb-4">
-                <h3>Our Project</h3>
+    <div className="container-fluid text-light" style={{          
+            background: `
+            linear-gradient(180deg, 
+              rgba(0, 0, 0, 1) 0%, 
+              rgba(0, 0, 0, 0.80) 50%, 
+              rgba(0, 0, 0, 0.0) 100%
+            )
+          `,
+            padding: '20px' ,
+            height: '100vh', 
+            width: '100vw' 
+          
+          }}>
+        <div className="row" style={{ margin: '100px'}}>
+            <div className="col-md-12 text-center mb-4" style={{ textShadow: '0 0 52px rgba(255, 255, 255, 1)',}}>
+                <h3 className="fw-bold fs-1 mb-auto ">Our Project</h3>
             </div>
         </div>
         <div className="row">
             <div className="col-md-4 mb-4">
-                <div className="card text-light" style={{ backgroundColor: '#4F4337', boxShadow: '0 0px 30px rgba(255, 255, 255, 0.8)' }}>
-                <img src="/nikki_p.jpeg" className="card-img-top img-responsive" alt="..." />
+                <div className="card text-light rounded-5" style={{backdropFilter: 'blur(16px)', backgroundColor: 'rgba(0, 0, 0, 0)', boxShadow: '0 4px 20px rgba(0, 0, 0, 0.4)'}}>
+                <img src="/bg0.jpeg" className="card-img-top img-responsive rounded-5" alt="..." />
                 <div className="card-body">
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <p className="card-text" style={{ textShadow: '0 0 20px rgba(255, 255, 255, 1)', }}>Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
                 </div>
                 </div>
             </div>
 
             <div className="col-md-4 mb-4">
-            <div className="card text-light" style={{ backgroundColor: '#4F4337', boxShadow: '0 0px 30px rgba(255, 255, 255, 0.8)' }}>
-                <img src="/nikki_p.jpeg" className="card-img-top" alt="..." />
+            <div className="card text-light rounded-5" style={{backdropFilter: 'blur(16px)', backgroundColor: 'rgba(0, 0, 0, 0)', boxShadow: '0 4px 20px rgba(0, 0, 0, 0.4)'}}>
+                <img src="/bg0.jpeg" className="card-img-top rounded-5" alt="..." />
                 <div className="card-body">
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <p className="card-text" style={{ textShadow: '0 0 20px rgba(255, 255, 255, 1)', }}>Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
                 </div>
                 </div>
             </div>
             <div className="col-md-4 mb-4">
-            <div className="card text-light" style={{ backgroundColor: '#4F4337', boxShadow: '0 0px 30px rgba(255, 255, 255, 0.8)' }}>
-                <img src="/nikki_p.jpeg" className="card-img-top" alt="..." />
+            <div className="card text-light rounded-5" style={{backdropFilter: 'blur(16px)', backgroundColor: 'rgba(0, 0, 0, 0)', boxShadow: '0 4px 20px rgba(0, 0, 0, 0.4)'}}>
+                <img src="/bg0.jpeg" className="card-img-top rounded-5" alt="..." />
                 <div className="card-body">
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <p className="card-text" style={{ textShadow: '0 0 20px rgba(255, 255, 255, 1)', }}>Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
                 </div>
                 </div>
             </div>
