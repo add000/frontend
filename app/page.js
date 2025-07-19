a90a5be4466d93bb9631a47e50bffe93acfc7a8a
add000 <addthai200@gmail.com>
2025-07-19 19:21:49 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
 import Carousel from './components/carousel';
 import Card from './components/card';
-import Nevigation from './components/navigation';
-import Footer from './components/footer';
+
+
+
+
 export default function Home() {
   return (
     <main>
