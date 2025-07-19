5a91e86713f3d1ca8a21db9d3d7357c64aa92d0c
add000 <addthai200@gmail.com>
2025-07-19 19:21:17 +0700

    Update page.js

--- app/about/page.js
+++ app/about/page.js
@@ -1,5 +1,7 @@
 export default function Page() {
     return (
-         <h1 className="ftext-4lx text-center">เกี่ยวกับเรา</h1>
-         );
+        <div>
+         <h1 className="ftext-4lx text-center" style={{height: '100vh', width: '100vw', padding: '20px', margin: '0 auto', marginTop: '100px'}}>เกี่ยวกับเรา</h1>
+        </div>
+    );
 }
