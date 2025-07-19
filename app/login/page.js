8df7f3a3b797e13a846a33cb34cebf4a8fb4a707
add000 <addthai200@gmail.com>
2025-07-19 19:21:46 +0700

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -1,7 +1,7 @@
 export default function LoginPage() {
   return (
-
-    <div className            = "container" style={{ maxWidth: '400px', marginTop: '50px' }}>
+    
+    <div className            = "container" style={{ maxWidth: '400px', padding: '20px', margin: '0 auto', marginTop: '100px' }}>
       <form className         = "border-none p-5 rounded-5" style={{backdropFilter: 'blur(16px)', backgroundColor: 'rgba(0, 0, 0, 0)', boxShadow: '0 4px 20px rgba(0, 0, 0, 0.4)'}}>
         <div className        = "mb-3">
 
